b748c8eebbc22b8a3f78db42ccf8bd43aa3a265b
Bruno <brunoartc@yahoo.com>
2018-05-22 17:51:16 -0300

    add initial firebase

---
+++ app/InfoInsper/app/src/main/java/com/br/insper/infoinsper/Form.java
@@ -0,0 +1,16 @@
+package com.br.insper.infoinsper;
+
+public class Form {
+
+    public String name;
+    public String email;
+    public String group;
+    public String issue;
+
+    public Form(String name, String email, String group, String issue) {
+        this.name = name;
+        this.email = email;
+        this.group = group;
+        this.issue = issue;
+    }
+}
